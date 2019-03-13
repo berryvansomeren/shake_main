ec669cef3ca3bfe30d90d535ed82a3d7636fe059
BerryVanSomeren <berryvansomeren@gmail.com>
2019-03-13 19:52:21 +0100

    moved camera controls into application, so that shake_graphics is no longer dependent on shake_hid

--- src/shake/main/application.cpp
+++ src/shake/main/application.cpp
@@ -6,26 +6,23 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "shake/content/content_manager.hpp"
 #include "shake/core/math/math.hpp"
-
-
+#include "shake/core/process_manager.hpp"
 #include "shake/core/types/lexical_cast.hpp"
 #include "shake/graphics/camera.hpp"
 #include "shake/graphics/context.hpp"
 #include "shake/graphics/draw.hpp"
 #include "shake/graphics/draw_text.hpp"
 #include "shake/graphics/gl.hpp"
+#include "shake/graphics/geometry/geometry_3d.hpp"
+#include "shake/graphics/geometry/voxel_grid.hpp"
+#include "shake/graphics/material/uniform.hpp"
 #include "shake/hid/keyboard.hpp"
 #include "shake/hid/mouse.hpp"
-
-#include "shake/core/process_manager.hpp"
-
 #include "shake/io/file_json.hpp"
-#include "shake/graphics/geometry/geometry_3d.hpp"
 
-#include "shake/graphics/material/uniform.hpp"
 
-#include "shake/content/content_manager.hpp"
 
 namespace shake {
 
@@ -51,9 +48,11 @@ Application::Application
     , m_frame_time_counter { }
     , m_fps_counter { }
     , m_process_manager { }
+    , m_content_manager { }
 {
     // graphics
     graphics::gl::init( m_window.get_glfw_gl_load_proc() );
+    content::init_gl( m_window.get_glfw_gl_load_proc() );
     graphics::set_current_screen_size( { screen_width, screen_height } );
 
     // hid
@@ -61,11 +60,10 @@ Application::Application
     Mouse::get_instance().init( m_window );
 
     // content
-    auto& content_manager = content::ContentManager::get_instance();
-    content_manager.init();
-    content_manager.set_shake_content_directory( content_directory );
-    content_manager.host_content_directory( game_content_directory );
-    content_manager.host_content_directory( content_directory );
+    m_content_manager.init( m_window.get_glfw_gl_load_proc() );
+    m_content_manager.set_shake_content_directory( content_directory );
+    m_content_manager.host_content_directory( game_content_directory );
+    m_content_manager.host_content_directory( content_directory );
 
     // processes 
     m_process_manager
@@ -73,15 +71,14 @@ Application::Application
         .add_process( []( float ) { Keyboard::get_instance().update(); } )
         .add_process( []( float ) { Mouse::get_instance().update(); } )
     ;
-
-
 }
 
 //----------------------------------------------------------------
 Application::~Application()
 {
-    // content
-    content::ContentManager::get_instance().destroy();
+    // make sure to destroy all content that's still in memory
+    // before destructing the correpsonding contexts
+    m_content_manager.destroy();
 }
 
 //----------------------------------------------------------------
@@ -128,23 +125,22 @@ void Application::run()
     auto running_average_counter = RunningAverageCounter< float, 120 > { };
     auto stopwatch = Stopwatch();
 
-    auto& content_manager = content::ContentManager::get_instance();
-    const auto voxel_content = io::file::json::read( content_manager.get_full_path( io::Path( "meshes/cube_map.json" ) ) );
+    const auto voxel_content = io::file::json::read( m_content_manager.get_full_path( io::Path( "meshes/cube_map.json" ) ) );
     const auto vertices      = io::file::json::read_as<std::vector<float>>( voxel_content, {"vertices"} );
     const auto skybox_geometry = std::make_shared<graphics::Triangles3D>( vertices );
-    const auto skybox_material = content_manager.get_or_load_material( io::Path { "materials/default_cube_map_material.json" } );
+    const auto skybox_material = m_content_manager.get_or_load<graphics::Material>( io::Path { "materials/default_cube_map_material.json" } );
     const auto render_pack_3d = graphics::RenderPack3D { skybox_geometry, skybox_material };
 
 
-    const auto default_primitive_3d_shader      = content_manager.get_or_load_shader( io::Path { "shaders/default_primitive_3d_shader.glsl" } );
+    const auto default_primitive_3d_shader      = m_content_manager.get_or_load<graphics::Shader>( io::Path { "shaders/default_primitive_3d_shader.glsl" } );
     const auto default_primitive_3d_material    = std::make_shared<graphics::Material>( default_primitive_3d_shader );
     
     const auto axes = std::vector<glm::vec3>{ { 1.0, 0.0, 0.0 }, { 0.0, 1.0, 0.0 }, { 0.0, 0.0, 1.0 } };
 
     const auto voxel_render_pack = graphics::RenderPack3D 
     { 
-        content_manager.get_or_load_voxel_grid( io::Path{ "voxel_models/default_voxel_model.vox" } ),
-        content_manager.get_or_load_material( io::Path { "materials/default_voxel_material.json" } )
+        m_content_manager.get_or_load<graphics::VoxelGrid>( io::Path{ "voxel_models/default_voxel_model.vox" } ),
+        m_content_manager.get_or_load<graphics::Material>( io::Path { "materials/default_voxel_material.json" } )
     };
 
     //----------------------------------------------------------------
@@ -154,10 +150,37 @@ void Application::run()
         // Get time delta
         const auto dt = stopwatch.get_and_reset();
 
-        if ( Keyboard::get_instance().is_down( Keyboard::Key::Escape ) )
+        const auto& keyboard = Keyboard::get_instance();
+        const auto& mouse = Mouse::get_instance();
+
+        if ( keyboard.get_instance().is_down( Keyboard::Key::Escape ) )
         {
             window.set_should_close();
         }
+
+        auto& camera = graphics::get_current_camera();
+
+        auto translation_velocity = float { 0.01f * dt };
+        if ( keyboard.is_down( Keyboard::Key::W ) ) { camera->get_transform().translate( -math::axis::z * translation_velocity );   }
+        if ( keyboard.is_down( Keyboard::Key::S ) ) { camera->get_transform().translate(  math::axis::z * translation_velocity );   }
+        if ( keyboard.is_down( Keyboard::Key::A ) ) { camera->get_transform().translate( -math::axis::x * translation_velocity );   }
+        if ( keyboard.is_down( Keyboard::Key::D ) ) { camera->get_transform().translate(  math::axis::x * translation_velocity );   }
+        if ( keyboard.is_down( Keyboard::Key::E ) ) { camera->get_transform().translate(  math::axis::y * translation_velocity );   }
+        if ( keyboard.is_down( Keyboard::Key::Q ) ) { camera->get_transform().translate( -math::axis::y * translation_velocity );   }
+
+        auto rotation_velocity = float { 0.01f * dt };
+        if( mouse.is_down( Mouse::Key::Left ) )
+        {
+            // to prevent introducing rotation by pitching and yawing, you do yaw relative to world
+            camera->get_transform().yaw_world       ( -mouse.get_diff_x() * rotation_velocity );
+            camera->get_transform().pitch_relative  ( -mouse.get_diff_y() * rotation_velocity );
+        }
+
+
+
+
+
+
 
         auto polygon_mode = graphics::gl::PolygonMode::Fill;
         if ( Keyboard::get_instance().is_down( Keyboard::Key::L ) )
